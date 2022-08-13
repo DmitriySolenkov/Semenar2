4e70db345635febded7ff8cf3cd2225ab6f3019e
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 09:38:27 +0300

    Ex16

---
+++ Ex16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int num1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 =Convert.ToInt32(Console.ReadLine());
+if (num1 % num2 == 0) Console.Write("ДА");
+else
+{
+   if (num2 % num1 ==0) Console.Write("ДА");
+   else Console.Write("НЕТ");
+}
