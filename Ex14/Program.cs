fc5bd58ce56bd2488f7c60fecb1ab7507c3f264b
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 09:33:50 +0300

    Ex14

---
+++ Ex14/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int num =Convert.ToInt32(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0) Console.Write("Число делится на 7 и 23");
+else Console.Write("Число НЕ делится на 7 и 23");
