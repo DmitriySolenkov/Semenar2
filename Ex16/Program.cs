91a6da8826124dafa5b0d80c4512808e3371acf3
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 09:41:08 +0300

    Ex16 Corrected

--- Ex16/Program.cs
+++ Ex16/Program.cs
@@ -2,9 +2,9 @@
 int num1 =Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 =Convert.ToInt32(Console.ReadLine());
-if (num1 % num2 == 0) Console.Write("ДА");
+if (num1*num1== num2) Console.Write("ДА");
 else
 {
-   if (num2 % num1 ==0) Console.Write("ДА");
+   if (num2*num2 ==num1) Console.Write("ДА");
    else Console.Write("НЕТ");
 }
