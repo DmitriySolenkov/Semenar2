67dd8a364bf0cc6dc0b58d7692dd24168b752c0b
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 09:30:21 +0300

    Ex12

---
+++ Ex12/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int num1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 =Convert.ToInt32(Console.ReadLine());
+int rem = num1 % num2;
+if (rem == 0) Console.Write("Кратно");
+else Console.Write("Некратно, остаток: "+ rem);
